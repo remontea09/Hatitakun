39aa607ef348c41cb9beed51507507d17f03738b
remontea <remonteaGit@gmail.com>
2025-11-27 11:08:39 +0900

    リファクタ

--- Assets/InGame/Script/GameManager.cs
+++ Assets/InGame/Script/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private HatitaController hatitakun;
     [SerializeField] private Goal goal;
     [SerializeField] private GameObject goalPanel;
+    [SerializeField] private PlayerDeath playerDeth;
     public GameObject gameOverUI;
     public static GameManager Instance;
 
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
         Application.targetFrameRate = 60;
         goalPanel.SetActive(false);
         goal.onGoal += OnGoal;
+        playerDeth.onGameOver += GameOver;
         Instance = this;
     }
 
