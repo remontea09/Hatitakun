3bb4737176abb4ba281bedd2b76e3bd372eb9ed4
remontea <remonteaGit@gmail.com>
2025-12-04 09:33:37 +0900

    GameOverが動作しない不具合と雨の降る間隔を修正

--- Assets/InGame/Script/GameManager.cs
+++ Assets/InGame/Script/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
         goalPanel.SetActive(false);
         goal.onGoal += OnGoal;
         playerGrowth.onGameEnd += GameOver;
-         
+        playerDeth.onGameOver += GameOver; 
     }
 
     private void OnGoal()
