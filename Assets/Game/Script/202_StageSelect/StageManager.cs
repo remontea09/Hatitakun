5d957434f29f0627dda589c550af2eacf4351fc2
koyukineko021543 <20243160@kcska.onmicrosoft.com>
2026-01-08 09:59:16 +0900

    表示できるか確認のボタン

--- Assets/Game/Script/202_StageSelect/StageManager.cs
+++ Assets/Game/Script/202_StageSelect/StageManager.cs
@@ -8,19 +8,25 @@ public class StageManager : MonoBehaviour
     [SerializeField] private Button Right;
     [SerializeField] private GameObject LeftButton;
     [SerializeField] private GameObject RightButton;
+
     [SerializeField] private int StagePanel = 1;//最初に表示するステージのページ
     [SerializeField] private TextMeshProUGUI stageN;//は矢印で増減する 文字
 
 
-    [SerializeField] private GameObject NumberPanel1;//コピーして入れたいステージを増やす
+    [SerializeField] private GameObject[] NumberPanel;//入れたいステージを増やす
+
 
     [SerializeField] private Button Dest;//消えるか確認用：終わったら消す
+    [SerializeField] private Button Miru1;//消えるか確認用：終わったら消す
+    [SerializeField] private Button Miru2;//消えるか確認用：終わったら消す
+
 
     //int Num = NumberManager.Number;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //矢印を消す
         if (StagePanel == 1)
         {
             LeftButton.SetActive(false);
@@ -30,9 +36,10 @@ public class StageManager : MonoBehaviour
         Right.onClick.AddListener(StageR);
 
 
-
         Dest.onClick.AddListener(Desty);//消えるか確認用：終わったら消す
 
+        Miru1.onClick.AddListener(Mi1);//消えるか確認用：終わったら消す
+        Miru2.onClick.AddListener(Mi2);//消えるか確認用：終わったら消す
 
     }
 
@@ -47,9 +54,22 @@ public class StageManager : MonoBehaviour
 
     private void Desty()//消えるか確認用：終わったら消す
     {
-        NumberPanel1.SetActive(false);
+        for (int i = 0; i < NumberPanel.Length; i++)
+        {
+            NumberPanel[i].SetActive(false);
+        }
 
 
+    }
+
+    private void Mi1()
+    {
+        NumberPanel[0].SetActive(true);
+    }
+
+    private void Mi2()
+    {
+        NumberPanel[1].SetActive(true);
     }
 
 
