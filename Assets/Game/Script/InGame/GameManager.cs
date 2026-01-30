658e99677b665f77884a16137d91dbcb434c938e
remontea <remonteaGit@gmail.com>
2026-01-30 11:46:52 +0900

    ステージ順次開放機能の実装

--- Assets/Game/Script/InGame/GameManager.cs
+++ Assets/Game/Script/InGame/GameManager.cs
@@ -18,10 +18,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI timeText;
     private float gameOverTime = 60;
 
+    private int stageNumber;
+
     private void Awake()
     {
         Time.timeScale = 1f;
         Application.targetFrameRate = 60;
+
+        TryGetStageNoFromSceneName(out stageNumber);
+
         goalPanel.SetActive(false);
         goal.onGoal += OnGoal;
         playerGrowth.onGameEnd += GameOver;
@@ -52,6 +57,7 @@ public class GameManager : MonoBehaviour
         goalManager.OnClear(level);
         howToPlayButton.SetActive(false);
         pauseButton.SetActive(false);
+        ClearStageService.SetClearScene(stageNumber - 1);
     }
 
     public void GameOver()
@@ -69,4 +75,14 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    private bool TryGetStageNoFromSceneName(out int stageNo)
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        stageNo = -1;
+
+        if (!sceneName.StartsWith("GameScene")) return false;
+
+        string numberPart = sceneName.Substring("GameScene".Length);
+        return int.TryParse(numberPart, out stageNo) && stageNo >= 1;
+    }
 }
